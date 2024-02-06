ab56e9e66658c9d9640ff1741d9826b430216e06
Jarvis <61008627+LastResortGamer@users.noreply.github.com>
2024-02-06 14:25:52 -0500

    Add files via upload

---
+++ mod.cpp
@@ -0,0 +1,8 @@
+name="Last Resort Vest Pack";
+author="LastResortGamer";
+logo="img\Logo.paa";
+logoOver="img\LogoHover.paa";
+tooltip="Last Resort Vest Pack v1.3";
+picture="img\LogoBig.paa";
+overview="This is a vest pack for any server. Join the discord and request a vest in '#arma-3-vest-request' channel";
+logoSmall="img\Logo.paa";
